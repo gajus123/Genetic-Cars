bc765e88e8c3cf09fcf0c6f85662a7a1b20b3fc2
Jakub Gajownik <gajus123@gmail.com>
2017-11-30 20:36:05 +0100

    Working Box2D and QT

--- SConstruct
+++ SConstruct
@@ -1,13 +1,20 @@
+import glob
+
 env = Environment()
 env['QT5DIR'] = 'H:\\Programy\\QT\\5.9.1\\msvc2015_64'
+#env.Append(QT5DIR=['H:\\Programy\\QT\\5.9.1\\msvc2015_64'])
 env.Tool('qt5')
+env.Append(CCFLAGS=['-fPIC','-std=c++11'])
+env.Append(LIBS=['boost_unit_test_framework'])
+env.Append(CPPPATH=['./../libs/'])
 env.EnableQt5Modules([
                       'QtGui',
                       'QtCore',
                       'QtNetwork',
-					  'QtWidgets'
+		      'QtWidgets'
                      ])
 build_path = './../build/'
 src_path = './'
 env.VariantDir(build_path, src_path, duplicate=0)
-env.Program(target = build_path+'GeneticCars', source = [build_path+'main.cpp'])
+source = [build_path+'main.cpp'] + glob.glob('Box2D/*.cpp')+glob.glob('Box2D/*/*.cpp')+glob.glob('Box2D/*/*/*.cpp')
+env.Program(target = build_path+'GeneticCars', source = source)
--- main.cpp
+++ main.cpp
@@ -1,13 +1,67 @@
-#include <iostream>
+#include "Box2D/Box2D.h"
 #include <QMainWindow>
 #include <QApplication>
 
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+//#define BOOST_TEST_DYN_LINK
+//#define BOOST_TEST_MODULE Main
+
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	
+	QApplication app(argc, argv);
 	QMainWindow window;
 	window.show();
-	
-	return a.exec();
+
+	B2_NOT_USED(argc);
+	B2_NOT_USED(argv);
+
+	b2Vec2 gravity(0.0f, -10.0f);
+
+	b2World world(gravity);
+
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+	b2PolygonShape groundBox;
+
+	groundBox.SetAsBox(50.0f, 10.0f);
+
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0f, 4.0f);
+	b2Body* body = world.CreateBody(&bodyDef);
+
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, 1.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+
+	fixtureDef.density = 1.0f;
+
+	fixtureDef.friction = 0.3f;
+
+	body->CreateFixture(&fixtureDef);
+
+	float32 timeStep = 1.0f / 60.0f;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	for (int32 i = 0; i < 60; ++i)
+	{
+		world.Step(timeStep, velocityIterations, positionIterations);
+
+		b2Vec2 position = body->GetPosition();
+		float32 angle = body->GetAngle();
+
+		//printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+	}
+
+	return app.exec();
 }
