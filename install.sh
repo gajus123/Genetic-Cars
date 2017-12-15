917efdb782724ea428ecd1c773d1e0ab650e57c0
Jakub Gajownik <gajus123@gmail.com>
2017-12-15 22:59:29 +0100

    Removed QT5 tool instalation

--- install.sh
+++ install.sh
@@ -16,19 +16,6 @@ sudo apt-get -y install scons
 echo "Instalacja Boost-test"
 sudo apt-get -y install libboost-test-dev
 
-#Pobieranie QT5-tool
-echo "Pobieranie Scons QT5-tool"
-if [ ! -n "$(ls -A ./site_scons/site_tools/qt5)" ]
-then
-	sudo wget https://bitbucket.org/dirkbaechle/scons_qt5/get/9caa36eb469d.zip
-	unzip 9caa36eb469d.zip -d 9caa36eb469d
-	mkdir -p ./site_scons/site_tools/qt5/
-	mv ./9caa36eb469d/dirkbaechle-scons_qt5-9caa36eb469d/* ./site_scons/site_tools/qt5/
-	rm -r 9caa36eb469d
-	sudo rm 9caa36eb469d.zip
-	rm -r ./site_scons/site_tools/qt5/test
-fi
-
 #Instalacja pkg-config
 echo "Instalacja pkg-config"
 sudo apt-get -y install pkg-config
