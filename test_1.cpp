3075cb13d06d80a1fc6d0f3ccdc4f0001ffecef2
Jakub Gajownik <gajus123@gmail.com>
2017-12-15 15:42:17 +0100

    Added example unit tests

---
+++ test_1.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestowanieSzkieletuAplikacji)
+
+BOOST_AUTO_TEST_CASE(JedneMniejszeOdDwoch)
+{
+	BOOST_CHECK(2<1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
