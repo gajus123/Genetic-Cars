d9d6c6b5a802b9983acf81028e01e3ff2e50caf5
gala377 <r.galczak@gmail.com>
2017-12-16 00:36:24 +0100

    Main brief docs

--- main.cc
+++ main.cc
@@ -1,3 +1,10 @@
+/*!
+ * @author Jakub Gajownik
+ * @date 14.12.17
+ *
+ * \brief main initializes and starts app and main window
+ */
+
 #include "Box2D/Box2D.h"
 #include <QApplication>
 #include "MainWindow.h"
