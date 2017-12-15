0ae3b349dc242b2bd4e9a284db257411bb0643fc
Jakub Gajownik <gajus123@gmail.com>
2017-12-15 21:02:41 +0100

    Removed installing Make

--- install.sh
+++ install.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-#Install gcc
-echo "Install GCC"
+#Instalacja gcc
+echo "Instalacja GCC"
 sudo apt-get -y install gcc
 
-#Install Make
-echo "Install Make"
-sudo apt-get -y install make
-
-#Install QT5
-echo "Install QT5"
+#Instalacja QT5
+echo "Instalacja QT5"
 sudo apt-get -y install qtbase5-dev
 
-#Install Scons
-echo "Install Scons"
+#Instalacja Scons
+echo "Instalacja Scons"
 sudo apt-get -y install scons
 
-#Install boost
-echo "Install Boost-test"
+#Instalacja boost
+echo "Instalacja Boost-test"
 sudo apt-get -y install libboost-test-dev
 
-#Download QT5 tool
-echo "Download Scons QT5 tool"
+#Pobieranie QT5-tool
+echo "Pobieranie Scons QT5-tool"
 if [ ! -n "$(ls -A ./site_scons/site_tools/qt5)" ]
 then
-	echo "No QT5 tool"
 	sudo wget https://bitbucket.org/dirkbaechle/scons_qt5/get/9caa36eb469d.zip
 	unzip 9caa36eb469d.zip -d 9caa36eb469d
 	mkdir -p ./site_scons/site_tools/qt5/
@@ -34,12 +29,12 @@ then
 	rm -r ./site_scons/site_tools/qt5/test
 fi
 
-#Install pkg-config
-echo "Install pkg-config"
+#Instalacja pkg-config
+echo "Instalacja pkg-config"
 sudo apt-get -y install pkg-config
 
-#Download Box2D
-echo "Download Box2D"
+#Pobiernie Box2D
+echo "Pobieranie Box2D"
 if [ ! -n "$(ls -A ./Box2D/)" ]
 then
 	sudo wget https://github.com/erincatto/Box2D/archive/master.zip
