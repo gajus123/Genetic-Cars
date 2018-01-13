74eb2e13b12bb712c011883b7c252888921a6a07
Jakub Gajownik <gajus123@gmail.com>
2018-01-13 16:41:50 +0100

    Cleanup

--- MainWindow.cc
+++ MainWindow.cc
@@ -20,53 +20,6 @@ MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags) : QMainWindow(par
 	layout->addWidget(&simulation_group_);
 	layout->addWidget(&file_group_);
 	layout->addWidget(&algorithm_group_);
-
-
-
-	/*setCentralWidget(&simulation_group_);
-	QVBoxLayout* layout = new QVBoxLayout();
-	
-	simulation_group_.setTitle("Symulacja");
-	simulation_group_.setLayout(layout);
-
-	layout->addWidget(&reset_button_);
-	layout->addWidget(&pause_button_);
-	
-	//Speed
-	QHBoxLayout* speedLayout = new QHBoxLayout();
-	QGroupBox* speedContainer = new QGroupBox();
-	speedContainer->setLayout(speedLayout);
-	speedContainer->setTitle("Prędkość symulacji");
-
-	layout->addWidget(speedContainer);
-
-	speedLayout->addWidget(&speed_decrease_button_);
-	speedLayout->addWidget(&speed_label_);
-	speedLayout->addWidget(&speed_increase_button_);
-
-	//Cars
-	QHBoxLayout* carsLayout = new QHBoxLayout();
-	QWidget* carsContainer = new QWidget();
-	carsContainer->setLayout(carsLayout);
-
-	layout->addWidget(carsContainer);
-
-	carsLayout->addWidget(&cars_count_label_);
-	carsLayout->addWidget(&cars_count_edit_);
-
-	reset_button_.setText("Resetuj");
-	pause_button_.setText("Pauza");
-	speed_decrease_button_.setText("-");
-	speed_increase_button_.setText("+");
-	speed_label_.setText("10");
-	speed_label_.setAlignment(Qt::AlignCenter);
-	cars_count_label_.setText("Ilość pojazdów:");*/
-
-	//setCentralWidget(&world_widget_);
-	//setCentralWidget(&simulation_view_);
-	//setCentralWidget(&reset_button_);
-	//resize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	world_widget_.start();
 }
 void MainWindow::setupSimulationInterface() {
 	QVBoxLayout* layout = new QVBoxLayout();
