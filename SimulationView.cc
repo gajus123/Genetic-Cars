f5e07a9e7b3a23577843799139d67cba1fd47749
Jakub Gajownik <gajus123@gmail.com>
2018-01-14 22:53:05 +0100

    Added zooming and displaying on the center of widget

--- SimulationView.cc
+++ SimulationView.cc
@@ -1,83 +1,82 @@
 #include "SimulationView.h"
 
 SimulationView::SimulationView(QWidget *parent, Qt::WindowFlags flags) :
-	QFrame(parent, flags) {
+	QFrame(parent, flags),
+	display_width_(10.0) {
+	width_multipier_ = this->width() / display_width_;
 }
 void SimulationView::paintEvent(QPaintEvent *event) {
 	QPainter p(this);
 	p.setRenderHint(QPainter::Antialiasing, true);
 
-	p.translate(-vehicles_[0].getPosition().x*100+200, -vehicles_[0].getPosition().y*100+200);
+	p.translate(-vehicles_[0].getPosition().x*width_multipier_ + display_center_x_, -vehicles_[0].getPosition().y*width_multipier_ + display_center_y_);
 	drawTrack(ground_[0], p);
 	drawCar(vehicles_[0], p);
 	p.translate(vehicles_[0].getPosition().x, vehicles_[0].getPosition().y);
 }
 void SimulationView::drawTrack(const Objects::Ground& track, QPainter& painter) {
 	painter.save();
-
-	painter.translate(track.getPosition().x*100, track.getPosition().y*100);
-	std::vector<QPointF> trackPoints;
-	for (const auto& point : track.getVertices()) {
-		trackPoints.push_back(QPointF(point.x*100, point.y*100));
-	}
-	for (unsigned int i = 1; i < trackPoints.size(); ++i) {
+	painter.translate(track.getPosition().x*width_multipier_, track.getPosition().y*width_multipier_);
+	auto trackPoints = track.getVertices();
+	for (unsigned int i = 1; i < track.getVertices().size(); ++i) {
 		drawTrackSegment(trackPoints[i - 1], trackPoints[i], painter);
 	}
-
 	painter.restore();
 }
+void SimulationView::resizeEvent(QResizeEvent *event) {
+	width_multipier_ = this->width() / display_width_;
+
+	display_center_x_ = this->width() * 0.5f;
+	display_center_y_ = this->height() * 0.66f;
+}
+void SimulationView::wheelEvent(QWheelEvent * event) {
+	display_width_ -= event->delta() / 120;
+	resizeEvent(nullptr);
+}
 void SimulationView::drawCar(const Objects::Vehicle& car, QPainter& painter) {
-	float angle = (car.getBody().getAngle() / M_PI) * 180.0f;
-	Objects::Vector2 position = car.getPosition();
-
-	//Tranform to the center of car
 	painter.save();
-	painter.translate(position.x*100, position.y*100);
-	painter.rotate(angle);
-
-	std::vector<QPointF> bodyShape;
-	for (const auto& point : car.getBody().getVertices()) {
-		bodyShape.emplace_back(std::move(QPointF(point.x*100, point.y*100)));
-	}
-	//Body
-	painter.setBrush(QBrush(Qt::green));
-	painter.drawPolygon(&bodyShape[0], bodyShape.size());
-
+	painter.translate(car.getPosition().x*width_multipier_, car.getPosition().y*width_multipier_);
+	drawBody(car.getBody(), painter);
 	painter.restore();
 
-	//Front wheel
-	painter.save();
-	painter.translate(car.getFrontWheel().getPosition().x*100, car.getFrontWheel().getPosition().y*100);
-
-	painter.setBrush(QBrush(QColor(255, 255, 102)));
-	painter.drawEllipse(QPointF(0, 0), car.getFrontWheel().getRadius()*100, car.getFrontWheel().getRadius()*100);
-
-
-	painter.restore();
-
-	//Back wheel
-	painter.save();
-	painter.translate(car.getBackWheel().getPosition().x*100, car.getBackWheel().getPosition().y*100);
-
-	painter.setBrush(QBrush(QColor(255, 255, 102)));
-	painter.drawEllipse(QPointF(0, 0), car.getBackWheel().getRadius()*100, car.getBackWheel().getRadius()*100);
-
-	painter.rotate(-car.getBackWheel().getAngle() / M_PI*180.0f);
-	painter.drawLine(0, 0, car.getBackWheel().getRadius()*100, 0);
-	painter.rotate(car.getBackWheel().getAngle() / M_PI*180.0f);
-
-	painter.restore();
+	drawWheel(car.getFrontWheel(), painter);
+	drawWheel(car.getBackWheel(), painter);
 
 	update();
 }
+void SimulationView::drawWheel(const Objects::Wheel& wheel, QPainter& painter) {
+	painter.save();
+	painter.translate(wheel.getPosition().x*width_multipier_, wheel.getPosition().y*width_multipier_);
 
-void SimulationView::drawTrackSegment(const QPointF& startPoint, const QPointF& endPoint, QPainter& painter) {
+	painter.setBrush(QBrush(QColor(255, 255, 102)));
+	painter.drawEllipse(QPointF(0, 0), wheel.getRadius()*width_multipier_, wheel.getRadius()*width_multipier_);
+
+	painter.rotate(-wheel.getAngle() / M_PI*180.0f);
+	painter.drawLine(0, 0, wheel.getRadius()*width_multipier_, 0);
+	painter.rotate(wheel.getAngle() / M_PI*180.0f);
+
+	painter.restore();
+}
+void SimulationView::drawBody(const Objects::Body& body, QPainter& painter) {
+	painter.rotate(body.getAngle() / M_PI * 180.0f);
+
+	std::vector<QPointF> bodyShape;
+	for (const auto& point : body.getVertices()) {
+		bodyShape.emplace_back(std::move(QPointF(point.x*width_multipier_, point.y*width_multipier_)));
+	}
+
+	painter.setBrush(QBrush(Qt::green));
+	painter.drawPolygon(&bodyShape[0], bodyShape.size());
+
+}
+
+void SimulationView::drawTrackSegment(const Objects::Vector2& startPoint, const Objects::Vector2& endPoint, QPainter& painter) {
 	painter.setBrush(QBrush(Qt::black));
 	const QPoint points[4] = {
-		QPoint(startPoint.x(), startPoint.y()),
-		QPoint(endPoint.x(), endPoint.y()),
-		QPoint(endPoint.x(), endPoint.y()+0.01f),
-		QPoint(startPoint.x(), startPoint.y()+0.01f)
+		QPoint(startPoint.x*width_multipier_, startPoint.y*width_multipier_),
+		QPoint(endPoint.x*width_multipier_, endPoint.y*width_multipier_),
+		QPoint(endPoint.x*width_multipier_, (endPoint.y+0.05f)*width_multipier_),
+		QPoint(startPoint.x*width_multipier_, (startPoint.y+0.05f)*width_multipier_)
 	};
 	painter.drawPolygon(points, 4);
 }
--- SimulationView.h
+++ SimulationView.h
@@ -15,6 +15,7 @@
 #include <QtMath>
 #include <QWidget>
 #include <QGLWidget>
+#include <QWheelEvent>
 
 class SimulationView : public QFrame {
 public:
@@ -22,13 +23,22 @@ public:
 	virtual void paintEvent(QPaintEvent *event);
 	std::vector<Objects::Ground> ground_;
 	std::vector<Objects::Vehicle> vehicles_;
+protected:
+	virtual void resizeEvent(QResizeEvent *event);
+	virtual void wheelEvent(QWheelEvent * event);
 private:
 	void drawTrack(const Objects::Ground& track, QPainter& painter);
 	void drawCar(const Objects::Vehicle& car, QPainter& painter);
-	void drawTrackSegment(const QPointF& startPoint, const QPointF& endPoint, QPainter& painter);
+	void drawWheel(const Objects::Wheel& wheel, QPainter& painter);
+	void drawBody(const Objects::Body& body, QPainter& painter);
+	void drawTrackSegment(const Objects::Vector2& startPoint, const Objects::Vector2& endPoint, QPainter& painter);
 
 	QTransform transform_;
-	Track track_;
+	
+	float display_width_;
+	float width_multipier_;
+	float display_center_x_;
+	float display_center_y_;
 };
 
 #endif
