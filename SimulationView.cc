8eb56a114095415a1591a06177c31d3b23aa6e4f
Jakub Gajownik <gajus123@gmail.com>
2018-01-14 20:06:24 +0100

    Cleanup

--- SimulationView.cc
+++ SimulationView.cc
@@ -1,63 +1,78 @@
 #include "SimulationView.h"
 
 SimulationView::SimulationView(QWidget *parent, Qt::WindowFlags flags) :
-	QFrame(parent, flags)
-	//ground_(Objects::Vector2(0, 0), 5.0f, { 0.0f, 1.0f, 0.0f, 3.0f })
-	/*wheel_(Objects::Vector2(1, 100), 1.0f)*/ {
-	transform_.translate(10,10);
+	QWidget(parent, flags) {
+	transform_.translate(0,0);
 }
 void SimulationView::paintEvent(QPaintEvent *event) {
 	QPainter p(this);
 	p.setRenderHint(QPainter::Antialiasing, true);
 	p.setTransform(transform_);
 
-	qDebug() << wheel_[0].getPosition().x << " " << wheel_[0].getPosition().y;
-
-	drawTrack(p);
-	drawCar(p);
+	drawTrack(ground_[0], p);
+	drawCar(vehicles_[0], p);
 }
-void SimulationView::drawTrack(QPainter& painter) {
-	auto trackPoints = track_.getTrackPoints();
+void SimulationView::drawTrack(const Objects::Ground& track, QPainter& painter) {
+	transform_.translate(track.getPosition().x, track.getPosition().y);
+	painter.setTransform(transform_);
+	std::vector<QPointF> trackPoints;
+	for (const auto& point : track.getVertices()) {
+		trackPoints.push_back(QPointF(point.x, point.y));
+	}
 	for (unsigned int i = 1; i < trackPoints.size(); ++i) {
 		drawTrackSegment(trackPoints[i - 1], trackPoints[i], painter);
 	}
+
+	transform_.translate(-track.getPosition().x, -track.getPosition().y);
 }
-void SimulationView::drawCar(QPainter& painter) {
-	Car car;
+void SimulationView::drawCar(const Objects::Vehicle& car, QPainter& painter) {
+	float angle = (car.getBody().getAngle() / M_PI) * 180.0f;
+	Objects::Vector2 position = car.getPosition();
 
 	//Tranform to the center of car
-	transform_.translate(car.getPosition().first, car.getPosition().second);
+	transform_.translate(position.x, position.y);
+	transform_.rotate(angle);
 	painter.setTransform(transform_);
 
+	std::vector<QPointF> bodyShape;
+	for (const auto& point : car.getBody().getVertices()) {
+		bodyShape.emplace_back(std::move(QPointF(point.x, point.y)));
+	}
 	//Body
 	painter.setBrush(QBrush(Qt::green));
-	std::vector<QPoint> carBody;
-	for (const auto& point : car.getShapePoints()) {
-		carBody.push_back(QPoint(point.first,point.second));
-	}
-	painter.drawPolygon(&carBody[0], car.getShapePoints().size());
+	painter.drawPolygon(&bodyShape[0], bodyShape.size());
 
-	//Wheels
-	painter.setBrush(QBrush(QColor(255,255,102)));
-	for (const auto& wheel : car.getWheels()) {
-		painter.drawEllipse(QPointF(wheel.first.first, wheel.first.second), wheel.second, wheel.second);
-	}
+	transform_.rotate(-angle);
+	transform_.translate(-position.x, -position.y);
 
-	//Transform to the previous position
-	transform_.translate(-car.getPosition().first, -car.getPosition().second);
+	//Front wheel
+	transform_.translate(car.getFrontWheel().getPosition().x, car.getFrontWheel().getPosition().y);
 	painter.setTransform(transform_);
-}
-/*void SimulationView::drawWheel(const b2Vec2& position, QPainter& painter) {
+
 	painter.setBrush(QBrush(QColor(255, 255, 102)));
-	painter.drawEllipse(QPointF(wheel.first.first, wheel.first.second), wheel.second, wheel.second);
-}*/
-void SimulationView::drawTrackSegment(const std::pair<float, float>& startPoint, const std::pair<float, float>& endPoint, QPainter& painter) {
+	painter.drawEllipse(QPointF(0, 0), car.getFrontWheel().getRadius(), car.getFrontWheel().getRadius());
+
+	transform_.translate(-car.getFrontWheel().getPosition().x, -car.getFrontWheel().getPosition().y);
+
+	//Back wheel
+	transform_.translate(car.getBackWheel().getPosition().x, car.getBackWheel().getPosition().y);
+	painter.setTransform(transform_);
+
+	painter.setBrush(QBrush(QColor(255, 255, 102)));
+	painter.drawEllipse(QPointF(0, 0), car.getBackWheel().getRadius(), car.getBackWheel().getRadius());
+
+	transform_.translate(-car.getBackWheel().getPosition().x, -car.getBackWheel().getPosition().y);
+
+	update();
+}
+
+void SimulationView::drawTrackSegment(const QPointF& startPoint, const QPointF& endPoint, QPainter& painter) {
 	painter.setBrush(QBrush(Qt::black));
 	const QPoint points[4] = {
-		QPoint(startPoint.first, startPoint.second),
-		QPoint(endPoint.first, endPoint.second),
-		QPoint(endPoint.first, endPoint.second-10.0f),
-		QPoint(startPoint.first, startPoint.second-10.0f)
+		QPoint(startPoint.x(), startPoint.y()),
+		QPoint(endPoint.x(), endPoint.y()),
+		QPoint(endPoint.x(), endPoint.y()+10.0f),
+		QPoint(startPoint.x(), startPoint.y()+10.0f)
 	};
 	painter.drawPolygon(points, 4);
 }
