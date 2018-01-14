5111a1a25a93a63c14f81556e07fed509acdca32
gala377 <r.galczak@gmail.com>
2018-01-14 21:35:37 +0100

    position update

--- MainWindow.cc
+++ MainWindow.cc
@@ -63,7 +63,7 @@ MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags) : QMainWindow(par
 	connect(&cars_count_edit_, SIGNAL(editingFinished()), this, SLOT(carsNumberChanged()));
 
 	Physics::ObjectsFactory::init(loop_);
-	simulation_view_.ground_.push_back(*GroundGenerator(1000, 20, 30.0f).genereteNew());
+	simulation_view_.ground_.push_back(*GroundGenerator(1000, 20, 30.0f).genereteNew(Vector2(0.0f, 0.0f)));
 	simulation_view_.vehicles_.push_back(Objects::Vehicle(Objects::Vector2(100, 40), {20.0, 20.0, 50.0, 28.0, 20.0, 28.0, 50.0, 20.0}, 40.0, 40.0));
 }
 void MainWindow::setupSimulationInterface() {
