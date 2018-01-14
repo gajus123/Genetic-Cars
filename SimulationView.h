a588ee2ca4095a0d54f3acf29efdf276cc60d826
Jakub Gajownik <gajus123@gmail.com>
2018-01-14 20:06:43 +0100

    Cleanup

--- SimulationView.h
+++ SimulationView.h
@@ -5,30 +5,29 @@
 #include "Car.h"
 #include "include\Ground.h"
 #include "include\Wheel.h"
+#include "include\Vehicle.h"
 #include "include\ObjectsFactory.h"
 #include <QFrame>
 #include <QTransform>
 #include <QPainter>
 #include <QPoint>
 #include <QDebug>
+#include <QtMath>
+#include <QWidget>
 
-class SimulationView : public QFrame {
+class SimulationView : public QWidget {
 public:
 	SimulationView(QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = 0);
 	virtual void paintEvent(QPaintEvent *event);
 	std::vector<Objects::Ground> ground_;
-	std::vector<Objects::Wheel> wheel_;
-	std::vector<b2Body*> ground2_;
+	std::vector<Objects::Vehicle> vehicles_;
 private:
-	void drawTrack(QPainter& painter);
-	void drawCar(QPainter& painter);
-	//void drawWheel(const b2Vec2& position, QPainter& painter);
-	void drawTrackSegment(const std::pair<float, float>& startPoint, const std::pair<float, float>& endPoint, QPainter& painter);
+	void drawTrack(const Objects::Ground& track, QPainter& painter);
+	void drawCar(const Objects::Vehicle& car, QPainter& painter);
+	void drawTrackSegment(const QPointF& startPoint, const QPointF& endPoint, QPainter& painter);
 
 	QTransform transform_;
 	Track track_;
-	//Objects::Ground ground_;
-	/*Objects::Wheel wheel_;*/
 };
 
 #endif
