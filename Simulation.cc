968fcd8da2ae6f7151473eebd227a317bcfaf69b
Jakub Gajownik <gajus123@gmail.com>
2018-01-15 14:08:25 +0100

    Added calculating statistics

--- Simulation.cc
+++ Simulation.cc
@@ -2,14 +2,19 @@
 
 Simulation::Simulation(QObject *parent) :
 	QObject(parent),
-	population_size_(10) {
+	watchdog_(this),
+	population_size_(CARS_NUMBER_ON_START) {
 
+	watchdog_.setInterval(2000);
+	connect(&watchdog_, SIGNAL(timeout()), this, SLOT(checkActivity()));
+	watchdog_.start();
 }
 void Simulation::clearVehicles() {
 	for (auto& vehicle : vehicles_) {
 		vehicle.destroy();
 	}
 	vehicles_.clear();
+	fitnesses_.clear();
 }
 void Simulation::newGround() {
 	if (ground_ != nullptr)
@@ -17,29 +22,42 @@ void Simulation::newGround() {
 		Physics::ObjectsFactory::getInstance().destroyBody(*ground_.get());
 	}
 	ground_.reset();
-	ground_ = std::shared_ptr<Objects::Ground>(GroundGenerator(1000, 1.2, 1.0f).genereteNew({ -1.0f, 5.0f }));
+	ground_ = std::shared_ptr<Objects::Ground>(GroundGenerator(TRACK_SEGMENTS, TRACK_SEGMENT_WIDTH, TRACK_SEGMENTS_DELTA).genereteNew({ 0.0f, 0.0f }));
 }
 void Simulation::newVehicles() {
 	clearVehicles();
-	
+
 	std::random_device rd;
 	std::mt19937 rng(rd());
-	std::uniform_real_distribution<float> uni(0.3, 1.0);
+	std::uniform_real_distribution<float> uni(0.3, 0.6);
 
 
 	for (std::size_t i = 0; i < population_size_; ++i) {
-		/*vehicles_.push_back(Objects::Vehicle(Objects::Vector2(0, 0.4), { 0.2f, 0.2f, 0.5f, 0.28f, 0.2f, 0.28f, 0.5f, 0.2f }, 0.35f, 0.35f));
-		vehicles_.push_back(Objects::Vehicle(Objects::Vector2(0, 0.4), { 0.2f, 0.2f, 0.5f, 0.28f, 0.2f, 0.28f, 0.5f, 0.2f }, 0.4f, 0.4f));*/
 		std::vector<float> points;
 		for (std::size_t i = 0; i < 8; ++i) {
 			points.push_back(uni(rng));
 		}
-		vehicles_.push_back(Objects::Vehicle(Objects::Vector2(0, 0.4), points, uni(rng), uni(rng)));
+		vehicles_.push_back(Objects::Vehicle(Objects::Vector2(CARS_START_X, CARS_START_Y), points, uni(rng), uni(rng)));
+		fitnesses_.emplace_back(0.0f);
 	}
 }
 void Simulation::reset() {
 	newGround();
 	newVehicles();
+}
+void Simulation::checkActivity() {
+	bool active = false;
+	for (std::size_t i = 0; i < vehicles_.size(); ++i) {
+		float xPosition = vehicles_[i].getPosition().x;
+		if (xPosition > fitnesses_[i]) {
+			fitnesses_[i] = xPosition;
+			active = true;
+		}
+	}
+	if (!active) {
+		emit roundEnd(fitnesses_);
+		newVehicles();
+	}
 }
 void Simulation::setPopulationSize(std::size_t newSize) {
 	this->population_size_ = std::min(newSize, MAXIMUM_CARS);
--- Simulation.h
+++ Simulation.h
@@ -10,11 +10,18 @@
 #include "include\ObjectsFactory.h"
 #include <cstdio>
 #include <cmath>
+#include <QTimer>
 
 class Simulation : public QObject {
 	Q_OBJECT
 
-		const std::size_t MAXIMUM_CARS = 30;
+	const std::size_t MAXIMUM_CARS = 30;
+	const std::size_t TRACK_SEGMENTS = 1000;
+	const float TRACK_SEGMENT_WIDTH = 1.2f;
+	const float TRACK_SEGMENTS_DELTA = 1.0f;
+	const std::size_t CARS_NUMBER_ON_START = 10;
+	const float CARS_START_X = 1.0f;
+	const float CARS_START_Y = -5.4f;
 public:
 	Simulation(QObject *parent = Q_NULLPTR);
 	void nextGeneration();
@@ -28,12 +35,18 @@ public:
 	void setPopulationSize(std::size_t newSize);
 public slots:
 	void reset();
+	void checkActivity();
+signals:
+	void roundEnd(std::vector<float> distances);
 private:
 	void clearVehicles();
 
 	std::vector<Objects::Vehicle> vehicles_;
+	std::vector<float> fitnesses_;
 	std::shared_ptr<Objects::Ground> ground_;
 	unsigned int population_size_;
+
+	QTimer watchdog_;
 };
 
 #endif
