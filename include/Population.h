9641bec12ff340a83c8a3c8db8a85fcd21fbcadc
Jakub Gajownik <gajus123@gmail.com>
2018-01-15 20:58:34 +0100

    Added elite specimen manipulation and constant

--- include/Population.h
+++ include/Population.h
@@ -13,34 +13,41 @@
 
 
 namespace Algorithm {
-	const int DEFAULT_ELITE_SPECIMEN = 4;
-	const float DEFAULT_MUTATION_RATE = 20;
-	const std::size_t DEFAULT_GENERATION_SIZE = 10;
+	const std::size_t DEFAULT_ELITE_SPECIMEN = 4;
+	const std::size_t MIN_ELITE_SPECIMEN = 1;
+	const float DEFAULT_MUTATION_RATE = 20.0f;
+	const std::size_t DEFAULT_POPULATION_SIZE = 10;
+	const std::size_t MAX_POPULATION_SIZE = 30;
+	const float MAX_MUTATION_RATE = 100.0f;
+	const float MIN_MUTATION_RATE = 0.0f;
 
 	class Population
 	{
 	public:
-		const unsigned int elite_specimen = DEFAULT_ELITE_SPECIMEN;
 
 		Population();
 		Population(const Population& other) = delete;
 		explicit Population(std::vector<Genotype> genotypes);
 
+		void reset();
 		void inflateRandom();
 		void nextPopulation();
 		std::vector<Genotype>& getGenotypes();
 
 		void setMutationRate(float rate);
+		void setEliteSpecimen(std::size_t elite_specimen);
 		void setNextGenerationSize(std::size_t population_size);
 
 		float getMutationRate() const;
+		std::size_t getEliteSpecimen() const;
 		std::size_t getNextGenerationSize() const;
 	private:
 		std::random_device rd_;
 		std::mt19937 rng_;
 
-		std::vector<Genotype> genotypes_;
 		float mutation_rate_;
+		unsigned int elite_specimen_;
+		std::vector<Genotype> genotypes_;
 		std::size_t next_population_size_;
 
 		std::pair<Genotype, Genotype> getNewChildren();
