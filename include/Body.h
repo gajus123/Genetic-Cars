05644ad18022994d1e8c22fd63e66b35e6c4d452
gala377 <r.galczak@gmail.com>
2018-01-15 18:47:46 +0100

    Docs

--- include/Body.h
+++ include/Body.h
@@ -1,5 +1,6 @@
 //
-// Created by igor on 13.01.18.
+// \author Rafa³ Galczak
+// \date 14.01.18
 //
 
 #ifndef GENETIC_CARS_BODY_H
@@ -12,27 +13,43 @@
 #include <cstdio>
 
 namespace Objects {
+
+	/*!
+		\class Body
+		\brief Vehicle body
+
+		Body is a b2PolygonShape. Shape is build on 8 vertices.
+		They can be treated as vectors with directions as compass rose.
+		Bodies differ from one other based on lenght of theese vectors, which
+		are later translated into points in the b2Body local coordinate space.
+	*/
     class Body : public Base {
     public:
-		static const unsigned int BODY_SEGMENTS;
+		static const unsigned int BODY_SEGMENTS; //!< Brief: Number of vertices wich body is created from
+		static const float BODY_FRICTION; //!< Brief: Default body friction
 
+		/*!
+			/param lenghts - length of 8 vectors creating body, going counter-clockwise,
+							 starting at upper one	
+			/param position - position in the world space to spawn body on
+		*/
         Body(std::vector<float> lengths, Vector2 position) : lengths(std::move(lengths)), Base(position) {
 			beforeBodySetUp();
-			createAndSetBody();
+			createAndSetBody();	
 			afterBodySetup();
 		};
 
         std::vector<Vector2> getVertices() const { return vertices; };
-        std::vector<Vector2> getCurrentVertices() const;
+        std::vector<Vector2> getCurrentVertices() const; //!< Brief: returns vertices shifted by current position
 
     protected:
         std::vector<Vector2> vertices;
         std::vector<float> lengths;
 
-        void beforeBodySetUp() override;
-        void calculateVertices();
+        void beforeBodySetUp() override; //!< Brief: Calculates and setus up vertives
+        void calculateVertices(); //!< Brief: Calculates b2PolygonShape vertices 
 
-        void createAndSetBody() override;
+        void createAndSetBody() override; //!< Brief: Creates b2PolygonShape based on vertices
     };
 
 }
--- include/Ground.h
+++ include/Ground.h
@@ -1,5 +1,6 @@
 //
-// Created by igor on 13.01.18.
+// \author Rafa³ Galczak
+// \date 14.01.18
 //
 
 #ifndef GENETIC_CARS_GROUND_H
@@ -10,22 +11,32 @@
 
 namespace Objects {
 
+	/*!
+		\class Ground
+		\brief b2Chain wrapper 
+
+		Ground is a static b2Chain body
+	*/
     class Ground : public Base {
     public:
+		/*!
+			/param position - position in the world space at which chain should start
+			/param x_distance - distance on x axis between neighbouring vertices
+			/param heights - vector of y coordinates of vertice
+		*/
 		Ground(Vector2 position, float x_distance, std::vector<float> heights);
 
-        std::vector<Vector2> getVertices() const { return vertices; }
-        std::vector<float> getHeights() const { return heights; }
-        float getXDistance() const { return x_distance; }
+        std::vector<Vector2> getVertices() const { return vertices_; }
+        std::vector<float> getHeights() const { return heights_; }
+        float getXDistance() const { return x_distance_; }
     protected:
-        std::vector<Vector2> vertices;
+        std::vector<Vector2> vertices_;
 
-        std::vector<float> heights;
-        float x_distance;
+        std::vector<float> heights_;
+        float x_distance_;
 
-        void beforeBodySetUp() override;
-
-        void createAndSetBody() override;
+        void beforeBodySetUp() override; //!< Brief: sets up chain vertices from x_distance and heights
+        void createAndSetBody() override; //!< Brief: Creates b2Chain static b2Body using Physics::ObjectFactory
     };
 
 }
