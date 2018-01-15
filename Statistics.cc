132dbc80de133b5a4b3184e24040ac32af621b4c
Jakub Gajownik <gajus123@gmail.com>
2018-01-15 14:09:04 +0100

    Added calculating statistics

--- Statistics.cc
+++ Statistics.cc
@@ -1,1 +1,20 @@
 #include "Statistics.h"
+
+void Statistics::calculateStatistics(std::vector<float> distances) {
+	float best = *std::max_element(distances.begin(), distances.end());
+	float worst = *std::min_element(distances.begin(), distances.end());
+	float average = 0.0f;
+	float median = 0.0f;
+
+	for (const auto& distance : distances) {
+		average += distance;
+	}
+	average /= distances.size();
+
+	if (distances.size() % 2 == 1)
+		median = distances[distances.size() / 2];
+	else
+		median = (distances[distances.size() / 2] + distances[distances.size() / 2 - 1]) / 2.0f;
+
+	emit newValues(best, worst, average, median);
+}
--- Statistics.h
+++ Statistics.h
@@ -2,9 +2,16 @@
 #define STATISTICS_H
 
 #include <QObject>
+#include <cstdio>
+#include <algorithm>
 
 class Statistics : public QObject{
 	Q_OBJECT
+
+public slots:
+	void calculateStatistics(std::vector<float> distances);
+signals:
+	void newValues(float best, float worst, float average, float median);
 };
 
 #endif
