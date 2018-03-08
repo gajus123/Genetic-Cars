a6ebea37e502b02244b5b2073176f8b138377ac8
Jakub Gajownik <gajus123@gmail.com>
2018-03-08 23:29:48 +0100

    Removed world from class

--- Loop.cc
+++ Loop.cc
@@ -7,13 +7,9 @@
 
 namespace Physics {
 
-    Loop::Loop(b2Vec2 gravity, int32 velocity_iterations, int32 position_iterations) :
+    Loop::Loop() :
 			QObject(),
-            world(new b2World(gravity)),
-			normal_time_step_(TIME_STEP),
-            time_step_(TIME_STEP),
-            velocity_iterations(velocity_iterations),
-            position_iterations(position_iterations) {
+			interval_(PHYSICS_FRAME_TIME) {
 		timer_.setInterval(PHYSICS_FRAME_TIME);
 		timer_.start();
 		connect(&timer_, SIGNAL(timeout()), this, SLOT(update()));
@@ -23,23 +19,19 @@ namespace Physics {
 		timer_.stop();
     }
 
-    b2World* Loop::getWorld() {
-        return world;
-    }
-
 	void Loop::update() {
-		world->Step(
-			time_step_,
-			velocity_iterations,
-			position_iterations
-		);
+		emit step(interval_);
 	}
 
 	void Loop::start() {
 		timer_.start();
 	}
 	void Loop::setTimeSpeed(float time_speed) {
-		time_step_ = normal_time_step_ * time_speed;
+		float new_interval = static_cast<float>(PHYSICS_FRAME_TIME) * time_speed;
+		interval_ = static_cast<unsigned int>(new_interval);
+		//std::cout << "Interval: " << interval_ << "\n";
+		//timer_.setInterval(interval_);
+		//time_step_ = normal_time_step_ * time_speed;
 	}
 
 }
--- Loop.h
+++ Loop.h
@@ -7,18 +7,13 @@
 #define GENETIC_CARS_PHYSICS_H
 
 #include <QTimer>
-#include <QDebug>
 #include <QObject>
-
-#include "Box2D/Box2D.h"
+#include <iostream>
 
 
 namespace Physics {
 
-	const b2Vec2 DEFAULT_GRAVITY = b2Vec2(0.0f, 9.8f); //!  Default world gravity
-	const float32 TIME_STEP = 1.0f / 60.0f; //!  Physics simulation world step
-	const int32 VELOCITY_ITERATIONS = 8; //!  b2World param - affects simulation accuracy, more better, but works slower
-	const int32 POSITION_ITERATIONS = 8; //!  b2Wordl param - affects simulation accuracy, more better. but works slower
+	const int PHYSICS_FRAME_TIME = 16;
 
 	/*!
 		\class Loop
@@ -34,35 +29,20 @@ namespace Physics {
 		Q_OBJECT
 
     public:
-		/*!
-			\param gravity - b2World gravity param
-			\param time_step - b2World.Step time step param
-			\param velocity_iterations - b2World.Step velocity_iterations param
-			\param position_iterations - b2World.Step position_iterations param
-		*/
-        explicit Loop(b2Vec2 gravity = DEFAULT_GRAVITY,
-                      int32 velocity_iterations = VELOCITY_ITERATIONS,
-                      int32 position_iterations = POSITION_ITERATIONS);
-        Loop(const Loop &) = delete;
-        Loop(Loop &&) = delete;
+        Loop();
+        Loop(const Loop&) = delete;
+        Loop& operator=(const Loop&) = delete;
 
 		void start(); //!  Starts a physics simulation
         void stop(); //!  Pauses a physisc simulation
 		void setTimeSpeed(float time_speed); //!  Sets time speed multiplier - 1.0 is normal speed
-
-        b2World* getWorld();
+	signals:
+		void step(unsigned int time);
 	private slots:
 		void update(); //!  Performs single physics loop iteration
     private:
-		int PHYSICS_FRAME_TIME = 16;
 		QTimer timer_;
-
-        b2World* world;
-        b2Vec2 gravity;
-        float32 time_step_;
-		float32 normal_time_step_;
-        int32 velocity_iterations;
-        int32 position_iterations;
+		unsigned int interval_;
     };
 
 }
