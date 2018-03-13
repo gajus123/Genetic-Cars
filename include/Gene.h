d8fa2e0f9d3fdeb56e386cc54ee8f5d8b0f1f39a
Jakub Gajownik <gajus123@gmail.com>
2018-03-13 22:41:53 +0100

    Added empty class

---
+++ include/Gene.h
@@ -0,0 +1,15 @@
+//
+// \author Jakub Gajownik
+// \date 13.03.18
+//
+
+#ifndef _GENETIC_CARS_GENE_H_
+#define _GENETIC_CARS_GENE_H_
+
+template<typename T>
+class Gene {
+public:
+private:
+};
+
+#endif
---
+++ src/Gene.cc
@@ -0,0 +1,6 @@
+//
+// \author Jakub Gajownik
+// \date 13.03.18
+//
+
+#include "include\Gene.h"
