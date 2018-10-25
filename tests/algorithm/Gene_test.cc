45339e4422b7b21143e0670a16f1e70ff6d32ac4
Jakub Gajownik <gajus123@gmail.com>
2018-10-25 23:00:43 +0200

    Removed old tests

--- tests/algorithm/Gene_test.cc
+++ tests/algorithm/Gene_test.cc
@@ -6,33 +6,14 @@
 #include <boost/test/unit_test.hpp>
 #include <limits>
 
-/*#include <Gene.h>
+#include <algorithm/Gene.hpp>
 
 BOOST_AUTO_TEST_SUITE(GeneTests);
 
 BOOST_AUTO_TEST_CASE(gene_constructor_and_value)
 {
-	Gene<float> g(0.925f, 0.0f, 1.0f);
-	BOOST_CHECK_EQUAL(g.value(), 0.925f);
+	Algorithm2::Gene g(412441);
+	BOOST_CHECK_EQUAL(g.value(), 412441);
 }
 
-BOOST_AUTO_TEST_CASE(gene_nkb)
-{
-	Gene<float> g(0.00003f, 0.0f, 1.0f);
-	BOOST_CHECK_EQUAL(g.nkb(), std::uint32_t(128849));
-}
-
-BOOST_AUTO_TEST_CASE(gene_grey)
-{
-	Gene<float> g(0.0001f, -100.0f, 100.0f);
-	BOOST_CHECK_EQUAL(g.nkb(), std::uint32_t(2147485696));
-}
-
-BOOST_AUTO_TEST_CASE(gene_cross)
-{
-	Gene<int> g1(2352331797, std::numeric_limits<int>::lowest(), std::numeric_limits<int>::max());
-	Gene<int> g2(41235398, std::numeric_limits<int>::lowest(), std::numeric_limits<int>::max());
-	BOOST_CHECK_EQUAL(g1.cross(g2).value(), 3276759583);
-}
-
-BOOST_AUTO_TEST_SUITE_END();*/
+BOOST_AUTO_TEST_SUITE_END();
